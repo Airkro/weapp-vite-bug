ddcc7f8dee155dd51ed6396c5ae951bf738231cc
Eric Chen <airkro@qq.com>
2026-07-24 13:57:12 +0800

    chore: update

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,25 @@ import { createRouter } from 'wevu/router'
 
 createApp({
   setup() {
-    createRouter({
+    const router = createRouter({
       routes: pages.map(page => ({
         path: page,
         name: page.split('/')[1],
       })),
     })
 
+    router.beforeEach((to, from) => {
+      console.log('beforeEach', to, from)
+    })
+
+    router.afterEach((to, from) => {
+      console.log('afterEach', to, from)
+    })
+
     return {}
   },
+})
+
+wx.onAppRoute((io) => {
+  console.log('onAppRoute', io)
 })
